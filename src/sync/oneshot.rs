1651b77f333866f3611667922076a279da3bb4ae
Adam Killam <adammkillam@gmail.com>
2025-08-30 04:44:01 -0230

    Prefer transparent wrapper struct

--- src/sync/oneshot.rs
+++ src/sync/oneshot.rs
@@ -15,21 +15,20 @@ use tokio_stream::Stream;
 /// [`tokio::sync::oneshot::Receiver`]: struct@tokio::sync::oneshot::Receiver
 /// [`Stream`]: trait@crate::Stream
 #[derive(Debug)]
-pub struct ReceiverStream<T> {
-    inner: Receiver<T>,
-}
+#[repr(transparent)]
+pub struct ReceiverStream<T>(pub Receiver<T>);
 
 impl<T> ReceiverStream<T> {
     /// Create a new `ReceiverStream`.
     #[inline(always)]
     pub fn new(recv: Receiver<T>) -> Self {
-        Self { inner: recv }
+        Self(recv)
     }
 
     /// Get back the inner `Receiver`.
     #[inline(always)]
     pub fn into_inner(self) -> Receiver<T> {
-        self.inner
+        self.0
     }
 
     /// Closes the receiving half of a channel without dropping it.
@@ -44,7 +43,7 @@ impl<T> ReceiverStream<T> {
     /// [`Permit`]: struct@tokio::sync::mpsc::Permit
     #[inline(always)]
     pub fn close(&mut self) {
-        self.inner.close();
+        self.0.close();
     }
 }
 
@@ -52,7 +51,7 @@ impl<T> Stream for ReceiverStream<T> {
     type Item = T;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        match Pin::new(&mut self.inner).poll(cx) {
+        match Pin::new(&mut self.0).poll(cx) {
             Poll::Ready(Ok(item)) => Poll::Ready(Some(item)),
             Poll::Ready(Err(_)) => Poll::Ready(None),
             Poll::Pending => Poll::Pending,
@@ -62,13 +61,13 @@ impl<T> Stream for ReceiverStream<T> {
 
 impl<T> AsRef<Receiver<T>> for ReceiverStream<T> {
     fn as_ref(&self) -> &Receiver<T> {
-        &self.inner
+        &self.0
     }
 }
 
 impl<T> AsMut<Receiver<T>> for ReceiverStream<T> {
     fn as_mut(&mut self) -> &mut Receiver<T> {
-        &mut self.inner
+        &mut self.0
     }
 }
 
