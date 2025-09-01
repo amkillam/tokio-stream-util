3cf6bc521045bb67af2fc4219e158dfee8fc7aff
Adam Killam <adammkillam@gmail.com>
2025-08-31 23:19:49 -0230

    Forward methods for wrapped structs

--- src/sync/oneshot.rs
+++ src/sync/oneshot.rs
@@ -7,7 +7,7 @@ use core::{
     pin::Pin,
     task::{Context, Poll},
 };
-use tokio::sync::oneshot::Receiver;
+use tokio::sync::oneshot;
 use tokio_stream::Stream;
 
 /// A wrapper around [`tokio::sync::oneshot::Receiver`] that implements [`Stream`].
@@ -16,19 +16,26 @@ use tokio_stream::Stream;
 /// [`Stream`]: trait@crate::Stream
 #[derive(Debug)]
 #[repr(transparent)]
-pub struct ReceiverStream<T>(pub Receiver<T>);
+pub struct Receiver<T>(pub oneshot::Receiver<T>);
 
-impl<T> ReceiverStream<T> {
-    /// Create a new `ReceiverStream`.
+pub(crate) struct ReceiverProj<'pin, T>(Pin<&'pin mut oneshot::Receiver<T>>);
+
+impl<T> Receiver<T> {
+    /// Create a new `Receiver`.
     #[inline(always)]
-    pub fn new(recv: Receiver<T>) -> Self {
+    pub fn new(recv: oneshot::Receiver<T>) -> Self {
         Self(recv)
     }
 
     /// Get back the inner `Receiver`.
     #[inline(always)]
-    pub fn into_inner(self) -> Receiver<T> {
+    pub fn into_inner(self) -> oneshot::Receiver<T> {
         self.0
+    }
+
+    pub(crate) fn project<'pin>(self: Pin<&'pin mut Self>) -> ReceiverProj<'pin, T> {
+        let this = self.get_mut();
+        ReceiverProj(Pin::new(&mut this.0))
     }
 
     /// Closes the receiving half of a channel without dropping it.
@@ -45,9 +52,41 @@ impl<T> ReceiverStream<T> {
     pub fn close(&mut self) {
         self.0.close();
     }
+
+    /// Forwards to the inner receiver's [`is_terminated`] method.
+    ///
+    /// [`is_terminated`]: tokio::sync::oneshot::Receiver::is_terminated
+    #[inline(always)]
+    pub fn is_terminated(&self) -> bool {
+        self.0.is_terminated()
+    }
+
+    /// Forwards to the inner receiver's [`is_empty`] method.
+    ///
+    /// [`is_empty`]: tokio::sync::oneshot::Receiver::is_empty
+    #[inline(always)]
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    /// Forwards to the inner receiver's [`try_recv`] method.
+    ///
+    /// [`try_recv`]: tokio::sync::oneshot::Receiver::try_recv
+    #[inline(always)]
+    pub fn try_recv(&mut self) -> Result<T, oneshot::error::TryRecvError> {
+        self.0.try_recv()
+    }
+
+    /// Forwards to the inner receiver's [`blocking_recv`] method.
+    ///
+    /// [`blocking_recv`]: tokio::sync::oneshot::Receiver::blocking_recv
+    #[inline(always)]
+    pub fn blocking_recv(self) -> Result<T, oneshot::error::RecvError> {
+        self.0.blocking_recv()
+    }
 }
 
-impl<T> Stream for ReceiverStream<T> {
+impl<T> Stream for Receiver<T> {
     type Item = T;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
@@ -59,20 +98,27 @@ impl<T> Stream for ReceiverStream<T> {
     }
 }
 
-impl<T> AsRef<Receiver<T>> for ReceiverStream<T> {
-    fn as_ref(&self) -> &Receiver<T> {
+impl<T> Future for Receiver<T> {
+    type Output = <oneshot::Receiver<T> as Future>::Output;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        self.project().0.poll(cx)
+    }
+}
+
+impl<T> AsRef<oneshot::Receiver<T>> for Receiver<T> {
+    fn as_ref(&self) -> &oneshot::Receiver<T> {
         &self.0
     }
 }
 
-impl<T> AsMut<Receiver<T>> for ReceiverStream<T> {
-    fn as_mut(&mut self) -> &mut Receiver<T> {
+impl<T> AsMut<oneshot::Receiver<T>> for Receiver<T> {
+    fn as_mut(&mut self) -> &mut oneshot::Receiver<T> {
         &mut self.0
     }
 }
 
-impl<T> From<Receiver<T>> for ReceiverStream<T> {
-    fn from(recv: Receiver<T>) -> Self {
+impl<T> From<oneshot::Receiver<T>> for Receiver<T> {
+    fn from(recv: oneshot::Receiver<T>) -> Self {
         Self::new(recv)
     }
 }
